3ed2a17412cab8d4338440128e7ca421692db839
徐家沛 <36961856@qq.com>
2020-03-20 18:10:02 +0800

    Update LCProgressHUD.m
    修改主线程

--- LCProgressHUD/LCProgressHUD.m
+++ LCProgressHUD/LCProgressHUD.m
@@ -8,22 +8,32 @@
 // 背景视图的宽度/高度
 #define BGVIEW_WIDTH 100.0f
 // 文字大小
-#define TEXT_SIZE    16.0f
+#define TEXT_SIZE     16.0f
+
+#define dispatch_main_async_safe_forLCD(block)\
+    if (dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL) == dispatch_queue_get_label(dispatch_get_main_queue())) {\
+        block();\
+    } else {\
+        dispatch_async(dispatch_get_main_queue(), block);\
+    }
 
 @implementation LCProgressHUD
 
-+ (instancetype)sharedHUD {
++ (instancetype)sharedHUD
+{
     static LCProgressHUD *hud;
     static dispatch_once_t onceToken;
+
     dispatch_once(&onceToken, ^{
         hud = [[self alloc] initWithView:[UIApplication sharedApplication].keyWindow];
     });
     return hud;
 }
 
-+ (void)showStatus:(LCProgressHUDStatus)status text:(NSString *)text {
-
++ (void)showStatus:(LCProgressHUDStatus)status text:(NSString *)text
+{
     LCProgressHUD *hud = [LCProgressHUD sharedHUD];
+
     [hud showAnimated:YES];
     [hud setShowNow:YES];
     hud.label.text = text;
@@ -35,10 +45,10 @@
 
     NSString *bundlePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"LCProgressHUD" ofType:@"bundle"];
 
-    switch (status) {
-
-        case LCProgressHUDStatusSuccess: {
-
+    switch (status)
+    {
+        case LCProgressHUDStatusSuccess:
+        {
             NSString *sucPath = [bundlePath stringByAppendingPathComponent:@"hud_success@2x.png"];
             UIImage *sucImage = [UIImage imageWithContentsOfFile:sucPath];
 
@@ -46,15 +56,15 @@
             UIImageView *sucView = [[UIImageView alloc] initWithImage:sucImage];
             hud.customView = sucView;
             [hud hideAnimated:YES afterDelay:1.0f];
-            
+
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [hud setShowNow:NO];
             });
         }
-            break;
+        break;
 
-        case LCProgressHUDStatusError: {
-
+        case LCProgressHUDStatusError:
+        {
             NSString *errPath = [bundlePath stringByAppendingPathComponent:@"hud_error@2x.png"];
             UIImage *errImage = [UIImage imageWithContentsOfFile:errPath];
 
@@ -62,21 +72,21 @@
             UIImageView *errView = [[UIImageView alloc] initWithImage:errImage];
             hud.customView = errView;
             [hud hideAnimated:YES afterDelay:1.0f];
-            
+
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [hud setShowNow:NO];
             });
         }
-            break;
+        break;
 
-        case LCProgressHUDStatusWaitting: {
-
+        case LCProgressHUDStatusWaitting:
+        {
             hud.mode = MBProgressHUDModeIndeterminate;
         }
-            break;
+        break;
 
-        case LCProgressHUDStatusInfo: {
-
+        case LCProgressHUDStatusInfo:
+        {
             NSString *infoPath = [bundlePath stringByAppendingPathComponent:@"hud_info@2x.png"];
             UIImage *infoImage = [UIImage imageWithContentsOfFile:infoPath];
 
@@ -84,21 +94,21 @@
             UIImageView *infoView = [[UIImageView alloc] initWithImage:infoImage];
             hud.customView = infoView;
             [hud hideAnimated:YES afterDelay:1.0f];
-            
+
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [hud setShowNow:NO];
             });
         }
-            break;
+        break;
 
         default:
             break;
     }
 }
 
-+ (void)showMessage:(NSString *)text {
-    
-    if (NSThread.currentThread.isMainThread) {
++ (void)showMessage:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         LCProgressHUD *hud = [LCProgressHUD sharedHUD];
         [hud showAnimated:YES];
         [hud setShowNow:YES];
@@ -110,75 +120,50 @@
         hud.label.font = [UIFont boldSystemFontOfSize:TEXT_SIZE];
         [[UIApplication sharedApplication].keyWindow addSubview:hud];
         //    [hud hide:YES afterDelay:1.0f];
-        
+
         [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(hide) userInfo:nil repeats:NO];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showMessage:text];
-        });
-    }
-
+    });
 }
 
-+ (void)showInfoMsg:(NSString *)text {
-    if (NSThread.currentThread.isMainThread) {
++ (void)showInfoMsg:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         [self showStatus:LCProgressHUDStatusInfo text:text];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showInfoMsg:text];
-        });
-    }
-    
+    });
 }
 
-+ (void)showFailure:(NSString *)text {
-    if (NSThread.currentThread.isMainThread) {
++ (void)showFailure:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         [self showStatus:LCProgressHUDStatusError text:text];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showFailure:text];
-        });
-    }
-    
+    });
 }
 
-+ (void)showSuccess:(NSString *)text {
-
-    if (NSThread.currentThread.isMainThread) {
++ (void)showSuccess:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         [self showStatus:LCProgressHUDStatusSuccess text:text];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showSuccess:text];
-        });
-    }
-    
+    });
 }
 
-+ (void)showLoading:(NSString *)text {
-    if (NSThread.currentThread.isMainThread) {
++ (void)showLoading:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         [self showStatus:LCProgressHUDStatusWaitting text:text];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showLoading:text];
-        });
-    }
-    
+    });
 }
 
-+ (void)hide {
-    if (NSThread.currentThread.isMainThread) {
++ (void)hide
+{
+    dispatch_main_async_safe_forLCD(^{
         [[LCProgressHUD sharedHUD] setShowNow:NO];
         [[LCProgressHUD sharedHUD] hideAnimated:YES];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD hide];
-        });
-    }
+    });
 }
 
-+ (void)showDetail:(NSString *)text{
-    
-    if (NSThread.currentThread.isMainThread) {
++ (void)showDetail:(NSString *)text
+{
+    dispatch_main_async_safe_forLCD(^{
         LCProgressHUD *hud = [LCProgressHUD sharedHUD];
         [hud showAnimated:YES];
         [hud setShowNow:YES];
@@ -190,16 +175,9 @@
         hud.detailsLabel.font = [UIFont boldSystemFontOfSize:TEXT_SIZE];
         [[UIApplication sharedApplication].keyWindow addSubview:hud];
         //    [hud hide:YES afterDelay:1.0f];
-        
+
         [NSTimer scheduledTimerWithTimeInterval:2.0f target:self selector:@selector(hide) userInfo:nil repeats:NO];
-    }else{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [LCProgressHUD showDetail:text];
-        });
-    }
-    
-
+    });
 }
-
 
 @end
