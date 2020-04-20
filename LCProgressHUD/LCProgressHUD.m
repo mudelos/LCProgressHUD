2453c92250946e271d225d2a7935d2679f764c5e
徐家沛 <36961856@qq.com>
2020-04-20 10:33:09 +0800

    Update LCProgressHUD.m
    修改菊花背景颜色

--- LCProgressHUD/LCProgressHUD.m
+++ LCProgressHUD/LCProgressHUD.m
@@ -26,6 +26,9 @@
 
     dispatch_once(&onceToken, ^{
         hud = [[self alloc] initWithView:[UIApplication sharedApplication].keyWindow];
+        hud.bezelView.color = [UIColor colorWithRed:0.0f/255.0f green:0.0f/255.0f blue:0.0f/255.0f alpha:0.3];
+        hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        hud.contentColor = [[UIColor whiteColor] colorWithAlphaComponent:0.9];
     });
     return hud;
 }
